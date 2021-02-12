3c78309256decbe1938069439c6b7579de09b2e4
Bryan <bryan@bluenestdigital.com>
2021-02-12 10:07:01 -0500

    Add base test case class

---
+++ src/RoostTestCase.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Creator: Bryan Mayor
+ * Company: Blue Nest Digital, LLC
+ * License: (Blue Nest Digital LLC, All rights reserved)
+ * Copyright: Copyright 2021 Blue Nest Digital LLC
+ */
+
+namespace Roost\Testing\PHPUnit\Helpers;
+
+use PHPUnit\Framework\TestCase;
+
+abstract class RoostTestCase extends TestCase
+{
+	protected function assertable($val) {
+		file_put_contents("assertable-last.out", var_export($val, true));
+		return $val;
+	}
+
+	protected function expectExceptionWrapper($exception, $callable) {
+		try {
+			$callable();
+		} catch(\Exception $ex) {
+			if($ex instanceof $exception) {
+				$this->addToAssertionCount(1);
+				return;
+			}
+		}
+
+		$this->fail(sprintf("Did not encounter exception %s", $exception));
+	}
+}
